f95e7a1bb73c822d52c69130fe5c063051b5cf2d
alejandro <leidyjohanasantacruzdaza03@gmail.com>
2023-09-20 15:14:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/moderndashboard/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.moderndashboard
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Tarjeta para la ropa
+        val clothingCard = findViewById<CardView>(R.id.clothingCard)
+        clothingCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, clothingActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Tarjeta para la electrónica
+        val elecName = findViewById<CardView>(R.id.elecName)
+        elecName.setOnClickListener {
+            val intent = Intent(this@MainActivity, ElectronicsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val  homecard = findViewById<CardView>(R.id.homecard)
+        homecard.setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        val beautycard = findViewById<CardView>(R.id.beautycard)
+        beautycard.setOnClickListener {
+            val intent = Intent(this@MainActivity, BeautyActivity::class.java)
+            startActivity(intent)
+        }
+
+        val  pharmacy= findViewById<CardView>(R.id.pharmacy)
+        pharmacy.setOnClickListener {
+            val intent = Intent(this@MainActivity, PharmacyActivity::class.java)
+            startActivity(intent)
+        }
+        val  groceries = findViewById<CardView>(R.id.groceries)
+        groceries.setOnClickListener {
+            val intent = Intent(this@MainActivity, GroceriesActivity::class.java)
+            startActivity(intent)
+        }
+        
+
+    }
+}
---
+++ app/src/main/java/com/example/moderndashboard/PharmacyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moderndashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PharmacyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pharmacy)
+    }
+}
